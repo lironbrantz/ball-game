77157bc97431810d3e4c1c89d41dc136fd50d53a
lironbrantz <52230166+lironbrantz@users.noreply.github.com>
2026-06-30 11:25:57 +0300

    Limit ball diameter

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ var gBallSize = 100
 function onBallClick() {
     gBallSize += 50
 
+    if (gBallSize > 400) {
+        gBallSize = 100
+    }
+
     const elBall = document.querySelector('.ball')
     elBall.style.width = gBallSize + 'px'
     elBall.style.height = gBallSize + 'px'
