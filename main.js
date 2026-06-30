a8348fe0b2364a4c200a7d5f96182e821371e8d8
lironbrantz <52230166+lironbrantz@users.noreply.github.com>
2026-06-30 11:13:58 +0300

    Grow ball on click

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 'use strict'
 
+var gBallSize = 100
+
 function onBallClick() {
-    console.log('Ball clicked')
+    gBallSize += 50
+
+    const elBall = document.querySelector('.ball')
+    elBall.style.width = gBallSize + 'px'
+    elBall.style.height = gBallSize + 'px'
+    elBall.innerText = gBallSize
 }
